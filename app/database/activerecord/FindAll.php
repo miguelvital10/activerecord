b86d0e84fc849927e4c1a1b696d7e2788ccd6236
Miguel Vital <miguelvital05@gmail.com>
2024-11-04 13:16:08 -0300

    (findAll) implementado findall no activeRecord

--- app/database/activerecord/FindAll.php
+++ app/database/activerecord/FindAll.php
@@ -1,16 +1,52 @@
 <?php
 
 namespace app\database\activerecord;
+
+use app\database\connection\Connection;
 use app\database\interfaces\ActiveRecordExecuteInterface;
 use app\database\interfaces\ActiveRecordInterface;
+use Exception;
+use Throwable;
 
-class FindAll{
+class FindAll implements ActiveRecordExecuteInterface{
+    
+    public function __construct(
+        private array $where = [],
+        private string|int $limit = '',
+        private string|int $offset = '',
+        private string $fields = '*',
+    ){
+        
+    }
+    
     public function execute(ActiveRecordInterface $activeRecordInterface)
     {
+        try {
+            $query = $this->createQuery($activeRecordInterface);
+
+            $connection = Connection::connect();
+
+            $prepare = $connection->prepare($query);
+            $prepare->execute($this->where);
+
+            return $prepare->fechAll();
+        } catch (Throwable $throw) {
+
+        }
     }
 
-    public function createQuery(ActiveRecordExecuteInterface $activeRecordInterface)
+    public function createQuery(ActiveRecordInterface $activeRecordInterface)
     {
+        if ((count($this->where) > 1)) {
+            throw new Exception('No where só pode passar um campo');
+        }
 
+        $where = array_keys($this->where);
+        $sql = "select {$this->fields} from {$activeRecordInterface->getTable()}";
+        $sql .= (!$this->where) ? '' : " where {$where[0]} = :{$where[0]}";
+        $sql .= (!$this->limit) ? '' : " limit {$this->limit}";
+        $sql .= (!$this->offset != '') ? "offset {$this->offset}" : "";
+
+        return $sql;
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,10 +2,11 @@
 
 require '../vendor/autoload.php';
 
+use app\database\activerecord\FindAll;
 use app\database\models\User;
 use app\database\activerecord\FindBy;
 
 $user = new User;
  
-var_dump($user->execute(new FindBy(field:'id', value:3, fields:'id,firstName')));
+var_dump ($user->execute(new FindAll(fields:'id, lastName')));
 
